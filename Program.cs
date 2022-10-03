1a308b4ae058bf6226ba07aea601c5fb01c1a357
konstantinlopatin <lopatinkos@gmail.com>
2022-10-03 21:26:08 +0300

    Решили задачу 8

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Введите число N = ");
+int N = int.Parse(Console.ReadLine()!);
+int x = 1;
+while (x < N) {
+    if (x % 2 == 0) { 
+        Console.WriteLine("{0} четное число", x);
+    }
+
+    x += 1;
+}
